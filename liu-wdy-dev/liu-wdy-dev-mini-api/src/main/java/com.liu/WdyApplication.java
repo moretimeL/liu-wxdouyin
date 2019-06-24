77b2bd288b24578776916ba918639e3c65f1280f
liujun <1057720671@qq.com>
2019-06-24 18:02:24 +0800

    update some poms

--- liu-wdy-dev/liu-wdy-dev-mini-api/src/main/java/com.liu/WdyApplication.java
+++ liu-wdy-dev/liu-wdy-dev-mini-api/src/main/java/com.liu/WdyApplication.java
@@ -1,11 +1,17 @@
 package com.liu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author ：liu jun
  * @date ：2019/6/24 16:55
  * @description：
  * @version:
  */
-
+@SpringBootApplication
 public class WdyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WdyApplication.class, args);
+    }
 }
